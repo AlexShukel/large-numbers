fd3b01a5c0284bd03030f8e02cce0277ff5e7f0c
Aleksandras Sukelovic <alex.shukel@gmail.com>
2023-02-26 10:55:15 +0200

    Implemented additionImpl function

---
+++ LargeNumber/test/additionImpl.test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alexs on 2023-02-25.
+//
+
+#include <gtest/gtest.h>
+#include "LargeNumber.h"
+
+// Demonstrate some basic assertions.
+TEST(A, B) {
+    ASSERT_EQ(1, 1);
+}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "LargeNumber.h"
 
+typedef uint8_t byte;
+
 int main() {
-    LargeNumber a("456141,154564111");
-    LargeNumber b("486456416,451321231121");
+    std::vector<byte> c1 = {13, 111};
+    std::vector<byte> c2 = {17, 3, 1};
 
-    LargeNumber sum = a + b;
+    LargeNumber<byte> num1(c1, false);
+    LargeNumber<byte> num2(c2, false);
 
-    std::cout << sum.toString() << '\n';
+    LargeNumber<byte> sum = num1 + num2;
+    std::cout << sum.getValue();
+
     return 0;
 }
