f4f24d5d165d2dbefd6c87de98263797ac18dd85
Aleksandras Sukelovic <alex.shukel@gmail.com>
2023-06-22 13:11:25 +0300

    Created first submission

---
+++ submissions/#1/demo.cpp
@@ -0,0 +1,51 @@
+//
+// Created by alexs on 2023-06-21.
+//
+
+#include "LargeFloat.h"
+#include "bubbleSort.h"
+#include <cassert>
+#include <iostream>
+
+using namespace LargeNumbers;
+
+int myFind(LargeFloat *numbers, const LargeFloat &number, size_t n) {
+    for (size_t i = 0; i < n; ++i) {
+        if (numbers[i] == number) {
+            return static_cast<int>(i);
+        }
+    }
+
+    return -1;
+}
+
+bool compare(const LargeFloat &a, const LargeFloat &b) {
+    return a < b;
+}
+
+void checkArrayIsSorted(LargeFloat *arr, size_t n) {
+    for (size_t i = 1; i < n; ++i) {
+        assert(arr[i - 1] <= arr[i]);
+    }
+}
+
+int main() {
+    LargeFloat numbers[] = {LargeFloat("512.0"), LargeFloat("0.0"), LargeFloat("1.5"), LargeFloat("-1.25")};
+
+    const size_t n = sizeof(numbers) / sizeof(LargeFloat);
+
+    assert(myFind(numbers, LargeFloat("1.5"), n) == 2);
+    assert(myFind(numbers, LargeFloat("0.5"), n) == -1);
+
+    bubbleSort(numbers, numbers + n, compare);
+
+    for (auto &x: numbers) {
+        std::cout << x.toString() << " ";
+    }
+    std::cout << std::endl;
+
+    checkArrayIsSorted(numbers, n);
+
+    std::cout << "Tests passed successfully!" << std::endl;
+    return 0;
+}
---
+++ submissions/#1/utils.h
@@ -0,0 +1,77 @@
+//
+// Created by alexs on 2023-05-14.
+//
+
+#ifndef ND1_UTILS_H
+#define ND1_UTILS_H
+
+#include <cstdint>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+
+uint8_t charToDigit(char character);
+
+char digitToChar(uint8_t digit);
+
+bool isStringZero(const std::string &number);
+
+uint8_t euclideanDivision(std::string &binary);
+
+template<class T>
+bool areCoefficientsEmpty(const std::vector<T> &coefficients) {
+    return std::all_of(coefficients.begin(), coefficients.end(), [](const T &c) {
+        return c == 0;
+    });
+}
+
+template<class T>
+class Equal {
+    T value;
+public:
+    explicit Equal(T value) : value(value) {
+    }
+
+    bool operator()(T another) {
+        return value == another;
+    }
+};
+
+template<class T, class V>
+size_t trimFront(T &container, V value) {
+    auto it = std::find_if_not(container.begin(), container.end(), Equal<V>(value));
+    size_t count = std::distance(container.begin(), it);
+    container.erase(container.begin(), it);
+    return count;
+}
+
+template<class T, class V>
+size_t trimBack(T &container, V value) {
+    auto it = std::find_if_not(container.rbegin(), container.rend(), Equal<V>(value));
+    size_t count = std::distance(container.rbegin(), it);
+    container.erase(it.base(), container.end());
+    return count;
+}
+
+template<class T, class V>
+size_t trimFrontNotLast(T &container, V value) {
+    size_t count = trimFront(container, value);
+    if (count > 0) {
+        container.insert(container.begin(), value);
+        return count - 1;
+    }
+    return count;
+}
+
+template<class T, class V>
+size_t trimBackNotLast(T &container, V value) {
+    size_t count = trimBack(container, value);
+    if (count > 0) {
+        container.insert(container.end(), value);
+        return count - 1;
+    }
+    return count;
+}
+
+#endif //ND1_UTILS_H
