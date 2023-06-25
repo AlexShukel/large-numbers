706207bbba9e6f0367f147c462fa441a6359430f
Aleksandras Šukelovič <alex.shukel@gmail.com>
2023-06-25 15:50:27 +0300

    Fixed pi representation

--- LargeFloat/src/LargeFloat.cpp
+++ LargeFloat/src/LargeFloat.cpp
@@ -20,12 +20,20 @@ namespace LargeNumbers {
             new Implementation(LargeFloatImplementation<CoefficientType>(
                     "0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001")));
 
-    // FIXME: make uint32_t version of pi
-    const LargeFloat LargeFloat::pi = LargeFloat(new Implementation(LargeFloatImplementation<CoefficientType>(LargeIntImplementation<CoefficientType>(
-            {141, 72, 86, 253, 82, 1, 121, 177, 12, 57, 203, 15, 222, 146, 191, 13, 183, 170, 153, 141, 112, 153, 104,
-             218, 216, 37, 2, 217, 215, 48, 247, 92, 10, 145, 101, 79, 142, 107, 43, 166, 86, 96, 104, 84, 190, 119, 19,
-             208, 56, 230, 33, 40, 69, 137, 108, 78, 236, 152, 250, 46, 8, 208, 49, 159, 41, 34, 56, 9, 164, 68, 115,
-             112, 3, 46, 138, 25, 19, 211, 8, 163, 133, 136, 106, 63, 36, 3}, false), 0)));
+    // uint_8_t pi representation
+//    const LargeFloat LargeFloat::pi = LargeFloat(new Implementation(LargeFloatImplementation<CoefficientType>(LargeIntImplementation<CoefficientType>(
+//            {141, 72, 86, 253, 82, 1, 121, 177, 12, 57, 203, 15, 222, 146, 191, 13, 183, 170, 153, 141, 112, 153, 104,
+//             218, 216, 37, 2, 217, 215, 48, 247, 92, 10, 145, 101, 79, 142, 107, 43, 166, 86, 96, 104, 84, 190, 119, 19,
+//             208, 56, 230, 33, 40, 69, 137, 108, 78, 236, 152, 250, 46, 8, 208, 49, 159, 41, 34, 56, 9, 164, 68, 115,
+//             112, 3, 46, 138, 25, 19, 211, 8, 163, 133, 136, 106, 63, 36, 3}, false), 0)));
+
+    // uint_32_t pi representation
+    const LargeFloat LargeFloat::pi = LargeFloat(
+            new Implementation(LargeFloatImplementation<CoefficientType>(LargeIntImplementation<CoefficientType>(
+                    {2073271439, 1392334408, 212957441, 3725577017, 3071131538, 1888328106, 3638192281, 3621323301,
+                     173864752, 2387568017, 1453730667, 3193202784, 953160567, 1160258022, 3964562569, 137296536,
+                     698298832, 2752067618, 57701188, 320440878, 2242054355, 608135816, 3}, false), 0)));
+
 
     LargeFloat::LargeFloat() : implementation(new Implementation()) {}
 
