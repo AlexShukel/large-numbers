0de79a0eb7c89414f60a4de48ff3b23b207363c1
Aleksandras Šukelovič <alex.shukel@gmail.com>
2023-06-25 17:03:30 +0300

    Updated submission

--- submissions/LargeNumbers/LargeIntImplementation.h
+++ submissions/LargeNumbers/LargeIntImplementation.h
@@ -21,6 +21,7 @@ namespace LargeNumbers {
 
         static constexpr size_t COEFFICIENT_BIT_SIZE = sizeof(T) * 8;
         static const T MAX_COEFFICIENT_VALUE;
+        static const uint64_t BASE;
 
         bool sign;
         std::vector<T> coefficients;
